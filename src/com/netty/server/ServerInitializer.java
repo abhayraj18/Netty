810c7ad38cefb36c2db949a829027e5e0a96d547
abhayraj18 <abhayraj.18@gmail.com>
2016-10-28 14:49:25 +0530

    deleted empty line

--- src/com/netty/server/ServerInitializer.java
+++ src/com/netty/server/ServerInitializer.java
@@ -14,7 +14,6 @@ public class ServerInitializer extends ChannelInitializer<SocketChannel> {
 
         pipeline.addLast("decoder", new StringDecoder());
         pipeline.addLast("encoder", new StringEncoder());
-
         pipeline.addLast("handler", new ServerHandler());
 	}
 	
